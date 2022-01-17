56f33039d2be2f5ee0f3aed208b646da9b37d041
Hekuan Yang <hekuan.yang@scrawlr.com>
2022-01-16 16:15:19 -0800

    init

--- javascripts/discourse/initializers/initialize-for-remove-ignored-users.js
+++ javascripts/discourse/initializers/initialize-for-remove-ignored-users.js
@@ -1,20 +1,49 @@
-import { apiInitializer } from "discourse/lib/api";
-import { on } from "discourse-common/utils/decorators";
+import {
+  apiInitializer
+} from "discourse/lib/api";
+import {
+  on
+} from "discourse-common/utils/decorators";
 
 export default apiInitializer("0.8", api => {
   const user = api.getCurrentUser();
   if (!user) return;
-  
-  const ignored = user.ignored_users;
+
+  // const ignored = user.ignored_users
+
+  const ignored = ['david', 'pekka_gaiser', 'sam'];
 
   if (ignored.length) {
+    //check isMobile
+    // const site = api.container.lookup("site:main");
+
+    // if (site.mobileView) return;
+
     api.modifyClass("component:topic-list", {
       @on("didReceiveAttrs")
+
+      // @on("didUpdateAttrs")
+
+      // pluginId: 'ignore plus',
+
       removeIgnoredUsers() {
         this.topics = this.topics.filter(
           topic => !ignored.includes(topic.posters[0].user.username)
         );
-      }
+
+     }
+   
+      
     });
+
+    // api.modifyClass("component:mobile-category-topic", {
+    //   @on("didReceiveAttrs")
+    //   removeIgnoredUsers() {
+    //     console.log(this);
+    //     this.topics = this.topics.filter(
+    //       topic => !ignored.includes(topic.posters[0].user.username)
+    //     );
+    //   }
+    // });
   }
 });
