7919359e22c0c031a685738d93778fb03c56f312
Hekuan Yang <hekuan.yang@scrawlr.com>
2022-01-25 18:03:58 -0800

    update css

--- javascripts/discourse/initializers/initialize-for-remove-ignored-users.js
+++ javascripts/discourse/initializers/initialize-for-remove-ignored-users.js
@@ -23,7 +23,7 @@ export default apiInitializer("0.11.1", api => {
 
   // get a list of ignored users
   const ignoredUsers = user.ignored_users;
-  // const ignoredUsers = ['codinghorror', 'david', 'pekka_gaiser', 'sam', 'adopilot'];
+  // const ignoredUsers = ['codinghorror', 'david', 'pekka_gaiser', 'johnsmith', 'adopilot'];
 
   if (!ignoredUsers?.length) {
     return
